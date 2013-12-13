fe06bcf3ec5a1b7f323a9585258ae102e6519a18
Carl Walsh <darthwalsh@gmail.com>
2013-12-13 09:58:41 -0800

    Fixed up ids fixes #5
    We can correctly give edge ids in error states now, and pick id winners
    based on length

--- SlitherLogic/logic.ts
+++ SlitherLogic/logic.ts
@@ -7,11 +7,11 @@ interface LogicElement {
 
 class Vertex implements LogicElement {
   public surroundings: Edge[] = [];
-  constructor(public name: String, public updateUI: () => void) {
+  constructor(public name: string, public updateUI: () => void) {
     Logic.vertices.push(this);
   }
 
-  connectedSelected(from: Edge): Edge {
+  connectedSelected(from: Edge): Edge[] {
     var fromV: Vertex;
     if (this === from.v1)
       if (this === from.v2)
@@ -24,14 +24,11 @@ class Vertex implements LogicElement {
       else
         throw "Edge didn't connect to this Vertex";
 
-    var found: Edge = null;
+    var found: Edge[] = [];
     for (var i = 0; i < this.surroundings.length; ++i) {
       var to = this.surroundings[i];
-      if (to.selected === true && to.v1 !== fromV && to.v2 !== fromV)
-        if (found === null)
-          found = to;
-        else
-          return null; // error state: we found multiple edges to go to!
+      if (to.selected === true && to !== from)
+        found.push(to);
     }
 
     return found;
@@ -60,10 +57,10 @@ class Edge implements LogicElement {
   public hints: Hint[] = [];
   private _selected: boolean = null;
   
-  static uniqueId = 0;
+  static uniqueId = 1; //don't start at 0 because falsy
   public id: number;
 
-  constructor(public name: String, public v1: Vertex, public v2: Vertex, public updateUI: () => void) {
+  constructor(public name: string, public v1: Vertex, public v2: Vertex, public updateUI: () => void) {
     v1.surroundings.push(this);
     v2.surroundings.push(this);
 
@@ -78,22 +75,56 @@ class Edge implements LogicElement {
     if (this._selected === newSelected)
       return;
 
+    var v1Edges: { [name: string]: Edge } = {};
+    this.allConnectedSearch(this.v2, v1Edges);
+    var v1Count = 0;
+    var v1Id: number = null;
+    for (var key in v1Edges) {
+      ++v1Count;
+      if (!v1Id) 
+        v1Id = v1Edges[key].id;
+    }
+
+    var v2Edges: { [name: string]: Edge } = {};
+    this.allConnectedSearch(this.v1, v2Edges);
+    var v2Count = 0;
+    var v2Id: number = null;
+    for (var key in v2Edges) {
+      ++v2Count;
+      if (!v2Id)
+        v2Id = v2Edges[key].id;
+    }
+
+    if (v1Count && v2Count) {
+      var reassignV2 = v1Count > v2Count;
+      var toAssignEdges = reassignV2 ? v2Edges : v1Edges;
+      if (newSelected === true) {
+        this.id = reassignV2 ? v1Id : v2Id;
+        if (v1Id === v2Id) { //just created a loop
+          //TODO did you win or not?
+        } else {
+          for (var key in toAssignEdges) {
+            toAssignEdges[key].id = this.id;
+            toAssignEdges[key].updateUI();
+          }
+        }
+      } else {
+        var toAssignId = Edge.uniqueId++;
+        for (var key in toAssignEdges) { //TODO refactor into above code?
+          toAssignEdges[key].id = toAssignId;
+          toAssignEdges[key].updateUI();
+        }
+      }
+    } else if (newSelected === true) {
+      this.id = v1Id || v2Id;
+      if(this.id === null)
+        this.id = Edge.uniqueId++;
+    }
+    //TODO#5 if a fork has become valid, fix up IDs
+    
     var oldSelected = this._selected;
     this._selected = newSelected;
 
-    var v1Edge = this.v1.connectedSelected(this);
-    var v2Edge = this.v2.connectedSelected(this);
-    if (v1Edge && v2Edge) {
-      var assignId = newSelected === true ? v1Edge.id : Edge.uniqueId++; //TODO pick winner based on longest
-      this.id = assignId;
-      v2Edge.setLoopId(this, assignId);
-    } else if (newSelected === true) {
-      if (v1Edge || v2Edge)
-        this.id = (v1Edge || v2Edge).id;
-      else
-        this.id = Edge.uniqueId++;
-    }
-    
     this.v1.updateUI();
     this.v2.updateUI();
     for (var i = 0; i < this.hints.length; ++i)
@@ -110,30 +141,23 @@ class Edge implements LogicElement {
     return this.selected === null ? null : true;
   }
 
-  setLoopId(from: Edge, newId: number) {
-    this.id = newId;
-    this.updateUI();
+  allConnectedSearch(from: Vertex, edges: {[name: string]: Edge}) {
+    var toV = from === this.v1 ? this.v2 : this.v1;
 
-    var v1Edge = this.v1.connectedSelected(this);
-    var v2Edge = this.v2.connectedSelected(this);
-    var nextEdge: Edge;
-    if (from === v1Edge && from === v2Edge)
-      throw 'Error state';
-    else if (from === v1Edge)
-      nextEdge = v2Edge;
-    else if (from === v2Edge)
-      nextEdge = v1Edge;
-    else
-      nextEdge = v1Edge || v2Edge;
-
-    if (nextEdge)
-      nextEdge.setLoopId(this, newId);
+    var toEdges = toV.connectedSelected(this);
+    for (var i = 0; i < toEdges.length; ++i) {
+      var to = toEdges[i];
+      if (!edges[to.name]) {
+        edges[to.name] = to;
+        to.allConnectedSearch(toV, edges);
+      }
+    }
   }
 }
 
 class Hint implements LogicElement {
   public num: number;
-  constructor(public name: String, public surroundings: Edge[], public updateUI: () => void) {
+  constructor(public name: string, public surroundings: Edge[], public updateUI: () => void) {
     for (var i = 0; i < surroundings.length; ++i)
       surroundings[i].hints.push(this);
   }
